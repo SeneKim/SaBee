6cfc83acaf0128a63d386975354212999cdd7f12
Sehee <sally001234@gmail.com>
2024-02-12 23:18:58 +0900

    Modify Gallery to use firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'screen/home.dart';
-import 'screen/beediary.dart';
+import 'firebase_options.dart';
 import 'screen/gallery.dart';
-import 'screen/alarm.dart';
-
 
 // 백그라운드에서 메시지를 처리하기 위한 핸들러
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -23,10 +21,9 @@ void main() async {
   );
 
   // FCM 토큰을 확인하고 데이터베이스에 저장하는 함수 호출
-  // await FirebaseNotificationHandler.initTokenAndSaveToDatabase();
+  await FirebaseNotificationHandler.initTokenAndSaveToDatabase();
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/screen/gallery.dart
+++ lib/screen/gallery.dart
@@ -1,75 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyGallery());
 }
 
-class MyGallery extends StatelessWidget {
+class MyGallery extends StatefulWidget {
   const MyGallery({super.key});
+
+  @override
+  State<MyGallery> createState() => _MyGalleryState();
+}
+
+class _MyGalleryState extends State<MyGallery> {
+  FirebaseStorage storage = FirebaseStorage.instance;
+
+  List<String> _imageUrls = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImages();
+  }
+
+  Future<void> _loadImages() async {
+    // 'images' 폴더 안의 파일 목록 가져오기
+    ListResult result = await storage.ref().child('images').listAll();
+    List<String> urls = [];
+    // 가져온 파일 목록에서 각 파일의 다운로드 URL을 얻어옴
+    for (Reference ref in result.items) {
+      String url = await ref.getDownloadURL();
+      urls.add(url);
+    }
+    setState(() {
+      _imageUrls = urls;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(children: [
-          Container(
-            width: 375,
-            height: 812,
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(color: Color(0xFFFFFBF5)),
-            child: Stack(
-              children: [
-                const Positioned(
-                  left: 151,
-                  top: 21,
-                  child: Text(
-                    '검출 갤러리',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFF2C2C2C),
-                      fontSize: 16,
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w600,
-                      height: 0,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 0,
-                  top: 64,
-                  child: Container(
-                    height: 3,
-                    width: 372,
-                    decoration: const BoxDecoration(
-                      color: Colors.black54,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 0,
-                  top: 70,
-                  child: Expanded(
-                    child: ListView(
-                      children: const [
-                        Text(
-                          '검출된 객체가 없습니다.',
-                          style: TextStyle(
-                            color: Color(0xFF2C2C2C),
-                            fontSize: 14,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w500,
-                            height: 0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+          appBar: AppBar(
+            backgroundColor: const Color(0xFFFFFCF6),
+            title: const Text(
+              '양봉 일지',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
             ),
+            centerTitle: true,
           ),
-        ]),
-      ),
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: 5.0,
+                    crossAxisSpacing: 5.0,
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: _imageUrls.length,
+                  itemBuilder: (context, index) {
+                    return Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.cover,
+                    );
+                  })
+            ],
+          )),
     );
+  }
+
+  /*Widget _imageGallery() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('images').snapshots(),
+        builder: (context, snapshot) {
+          return (snapshot.connectionState == ConnectionState.waiting)
+              ? const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                )
+              : GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 5.0,
+                      crossAxisSpacing: 5.0,
+                      crossAxisCount: 3),
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final url = snapshot.data!.docs[index]['url'];
+                    return Image.network(
+                      url,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    );
+                  });
+        });
+  }*/
+}
+
+class FireStoreDataBase {
+  String? downloadURL;
+
+  Future getData() async {
+    try {
+      await downloadURLExample();
+      return downloadURL;
+    } catch (e) {
+      debugPrint("Error - $e");
+      return null;
+    }
+  }
+
+  Future<void> downloadURLExample() async {
+    downloadURL = await FirebaseStorage.instance
+        .ref()
+        .child("Flutter.png")
+        .getDownloadURL();
+    debugPrint(downloadURL.toString());
   }
 }
